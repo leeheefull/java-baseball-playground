639c07bf2fe0bfae1602e4d9ccbc57a9cadcbf84
leeheechan <coolzz7@naver.com>
2021-06-05 18:36:12 +0900

    feat: 코드 리뷰 피드백 적용
    * minus, plus 정수 계산기능 추가
    * Constants class 생성

---
+++ src/main/java/calculator/utils/Constants.java
@@ -0,0 +1,5 @@
+package calculator.utils;
+
+public class Constants {
+    public static final int MINIMUM_SIZE = 2;
+}
--- src/main/java/calculator/utils/StringUtil.java
+++ src/main/java/calculator/utils/StringUtil.java
@@ -1,11 +1,9 @@
 package calculator.utils;
 
 public class StringUtil {
-    private static final String numberRegex = "^[0-9]*$";
-
-    public static final int MINIMUM_SIZE = 2;
+    private static final String numberRegex = "^[+-]?[0-9]*$";
 
     public static boolean isNumber(String value) {
-        return !value.matches(numberRegex);
+        return value.matches(numberRegex);
     }
 }
